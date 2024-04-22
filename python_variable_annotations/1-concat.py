2ccfc58b8dd76ace8cea108eeabe08fceea4446b
harishammache <hammache_haris@hotmail.fr>
2024-04-22 10:08:03 +0000

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+     function concat that takes a string str1 and a string str2
+    as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concatenate two string
+
+        argument1: first str
+        argument2: second str
+
+        result: the str concatenate
+    """
+    result = str1 + str2
+    return result
