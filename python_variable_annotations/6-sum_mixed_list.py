3bbdf6559c0cc762aab817d34cab77a25b51b25b
harishammache <hammache_haris@hotmail.fr>
2024-04-22 11:20:22 +0000

    change place of int and place

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
         Sum a list of integers and floats.
 
