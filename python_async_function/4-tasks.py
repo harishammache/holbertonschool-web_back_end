214ae489bf3b3b7743260da91ccd0d078d32c22b
harishammache <hammache_haris@hotmail.fr>
2024-04-23 11:11:30 +0000

    4. Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except
+    task_wait_random is being called.
+"""
+from typing import List, Task
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        Crée et retourne une liste d'objets asyncio.Task pour les coroutines wait_random.
+
+        Paramètres :
+            n (int) : Le nombre de fois d'appeler task_wait_random.
+            max_delay (int) : Le délai maximal en secondes pour chaque appel task_wait_random.
+
+        Retourne :
+            List[float] : Une liste contenant les résultats des coroutines.
+    """
+    tasks = []
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+
+    results = await asyncio.gather(*tasks)
+    return results
