b898ae878e18d5ec9eecfe59a1a82f79cd8c02d3
harishammache <hammache_haris@hotmail.fr>
2024-04-23 11:15:40 +0000

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -11,7 +11,6 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
         Crée et retourne une liste d'objets asyncio.Task pour
