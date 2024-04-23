fb096b5869dd04e115353665ba1d1918c460a2c2
harishammache <hammache_haris@hotmail.fr>
2024-04-23 11:14:47 +0000

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-    Take the code from wait_n and alter it into a new function task_wait_n.
+    Take the code from wait_n and alter it into
+    a new function task_wait_n.
     The code is nearly identical to wait_n except
     task_wait_random is being called.
 """
@@ -13,11 +14,13 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-        Crée et retourne une liste d'objets asyncio.Task pour les coroutines wait_random.
+        Crée et retourne une liste d'objets asyncio.Task pour
+        les coroutines wait_random.
 
         Paramètres :
             n (int) : Le nombre de fois d'appeler task_wait_random.
-            max_delay (int) : Le délai maximal en secondes pour chaque appel task_wait_random.
+            max_delay (int) : Le délai maximal en secondes
+            pour chaque appel task_wait_random.
 
         Retourne :
             List[float] : Une liste contenant les résultats des coroutines.
