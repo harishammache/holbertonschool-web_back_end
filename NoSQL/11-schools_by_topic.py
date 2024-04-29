6519503c0d383a82ed10f493c2b5c263630edc79
harishammache <hammache_haris@hotmail.fr>
2024-04-29 10:20:59 +0000

    11. Where can I learn Python?

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -10,6 +10,8 @@ def schools_by_topic(mongo_collection, topic):
         mongo_collection will be the pymongo collection object
         topic (string) will be topic searched
     """
+    results = []
 
     for result in mongo_collection.find({"topics": topic}):
-        return result
+        results.append(result)
+    return results
