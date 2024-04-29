8c69812f1c929de9c854966f506a2537addfbc2d
harishammache <hammache_haris@hotmail.fr>
2024-04-29 10:01:21 +0000

    10. Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+     changes all topics of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+        mongo_collection will be the pymongo collection object
+        name (string) will be the school name to update
+        topics (list of strings) will be the list of topics approached in the school
+    """
+    result = mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics }})
+    return result.modfied_count
