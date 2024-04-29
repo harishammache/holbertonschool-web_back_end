340e0e46dc444e0766243331a4f4017da6791e35
harishammache <hammache_haris@hotmail.fr>
2024-04-29 11:43:14 +0000

    pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """
-    Write a function named index_range that takes two integer arguments page and page_size
+    Write a function named index_range that takes
+    two integer arguments page and page_size
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-        return a tuple of size two containing a start index and an end index corresponding
-        to the range of indexes to return in a list for those particular pagination parameters.
+        return a tuple of size two containing a start index and
+        an end index corresponding
+        to the range of indexes to return in a list for those particular
+        pagination parameters.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
