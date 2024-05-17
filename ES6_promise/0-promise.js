b990274aed2fc2cb71e1e75580a80f3f62cda3d7
harishammache <hammache_haris@hotmail.fr>
2024-05-17 10:06:58 +0000

    export

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,9 @@
-export default function getResponseFromAPI () {
-  const myPromise = new Promise(
-    (resolve) => {
-      resolve();
-    }
-  );
-  return myPromise;
+function getResponseFromAPI () {
+  return new Promise((resolve) => {
+    // Asynchronous operations could be placed here
+    // For now, we will simply resolve the promise immediately
+    resolve('This is a response from the API');
+  });
 }
+
+export default getResponseFromAPI;
