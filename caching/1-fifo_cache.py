198fc006349e09e1c9d93e8081feb0061717d534
harishammache <hammache_haris@hotmail.fr>
2025-04-15 13:19:51 +0200

    feat(task1): Create a class FIFOCache that inherits from BaseCaching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    FIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Create a class FIFOCache that inherits from BaseCaching"""
+    def __init__(self):
+        """call the parent init"""
+        super().__init__()
+        self.key_save = []
+
+    def put(self, key, item):
+        """Add an item in the cache using FIFO replacement policy."""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            oldest_key = self.key_save.pop(0)
+            del self.cache_data[oldest_key]
+            print(f"DISCARD: {oldest_key}")
+        self.key_save.append(key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache by key."""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
