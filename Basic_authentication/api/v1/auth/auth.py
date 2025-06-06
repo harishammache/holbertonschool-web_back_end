5ead552186fbd8cf24772a06dc615562f7596e07
harishammache <hammache_haris@hotmail.fr>
2025-06-06 13:42:40 +0200

    feat(auth): enhance require_auth with path and slash normalization

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,17 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        
+        if not path.endswith('/'):
+            path = path + '/'
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """public method"""
