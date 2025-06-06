4ecbdd8e2ea699b9b3e9792d1902325e83c9ca26
harishammache <hammache_haris@hotmail.fr>
2025-06-06 15:31:50 +0200

    pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,22 +8,17 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """create a classe that inherits from Auth"""
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-         """add the method"""
-         if authorization_header is None:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+            ) -> str:
+        """add the method"""
+        if authorization_header is None:
             return None
-         
-         if not isinstance(authorization_header, str):
+
+        if not isinstance(authorization_header, str):
             return None
-         
-         if not authorization_header.startswith("Basic "):
+
+        if not authorization_header.startswith("Basic "):
             return None
-         
-         return authorization_header[6:].strip()
-    
 
-    
-
-         
-
-
+        return authorization_header[6:].strip()
