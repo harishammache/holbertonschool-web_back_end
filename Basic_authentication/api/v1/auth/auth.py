aad21d037d7952a13e9fdc5167f22a1e8d91206b
harishammache <hammache_haris@hotmail.fr>
2025-06-06 13:45:11 +0200

    pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,16 +12,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method"""
-        if path is None or excluded_paths is None:
+        if path is None or not excluded_paths:
             return True
-        
-        for excluded_path in excluded_paths:
-            if path == excluded_path:
-                return False
-        
+
         if not path.endswith('/'):
             path = path + '/'
-        
+
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path == excluded_path:
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
