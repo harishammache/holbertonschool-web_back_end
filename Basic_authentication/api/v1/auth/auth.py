65c648bb22a08dec7c1a400e4efde17664d93ed9
harishammache <hammache_haris@hotmail.fr>
2025-06-06 13:36:58 +0200

    pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,15 +10,14 @@ from typing import List, TypeVar
 class Auth:
     """class auth"""
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """public method"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """public method"""
         return None
