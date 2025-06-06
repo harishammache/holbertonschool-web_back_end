933a2154c5029a663474103355fd797bc9a01b8f
harishammache <hammache_haris@hotmail.fr>
2025-06-06 15:39:00 +0200

    feat(auth): add decode_base64_authorization_header method in BasicAuth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:].strip()
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+            ) -> str:
+        """Add the methode"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header,
+                                             validate=True)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
