9a34f1427b8aef3d5664817e1ba8bb39f6cb2173
harishammache <hammache_haris@hotmail.fr>
2025-06-25 12:02:58 +0200

    add an import

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -15,8 +15,9 @@ from api.v1.auth.session_auth import SessionAuth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth_type = os.getenv('AUTH_TYPE')
 auth = None
+auth_type = os.getenv('AUTH_TYPE')
+
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,7 @@
 """
 
 
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
