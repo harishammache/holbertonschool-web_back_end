81b686a65c74964ac94e4d1ad9d6159cfbdfb17a
harishammache <hammache_haris@hotmail.fr>
2025-06-25 12:37:43 +0200

    feat(auth): implement session creation with uuid4 and user_id mapping

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,19 @@
 
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """class SessionAuth that inherits from Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """instance method"""
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
