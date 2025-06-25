c6b716576b231bfb5984998e243176f2d8873018
harishammache <hammache_haris@hotmail.fr>
2025-06-25 16:16:38 +0200

    pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -10,8 +10,7 @@ Base = declarative_base()
 
 class User(Base):
     """User model"""
-    __tablename__='users'
-
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
