f5cfcb6c3caf32844dbd60903abf39f69d874029
harishammache <hammache_haris@hotmail.fr>
2025-06-25 12:54:03 +0200

    pycodestyle

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,7 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ instance method """
         if session_id is None:
