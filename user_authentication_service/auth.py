aa769e91f7fec672aa1a1ac3506f95729fcededd
harishammache <hammache_haris@hotmail.fr>
2025-06-26 16:02:24 +0200

    feat(auth): add valid_login method with bcrypt password verification

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -33,3 +33,12 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Valide user login credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
