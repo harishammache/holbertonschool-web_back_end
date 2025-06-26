4c5463a3a2756af0f0beb634ab1fa5ef00e3504a
harishammache <hammache_haris@hotmail.fr>
2025-06-26 16:56:02 +0200

    feat(api): add user login endpoint with session management

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,7 +3,7 @@
     basic Flask app
 """
 from auth import Auth
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 
 
 AUTH = Auth()
@@ -29,6 +29,21 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/session', methods=['POST'])
+def login() -> str:
+    '''session endpoint'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
