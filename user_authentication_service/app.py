a5f87dba4295148e0eb29886344eaf887d1781c8
harishammache <hammache_haris@hotmail.fr>
2025-06-26 15:46:42 +0200

    feat(api): implement POST /users endpoint for user registration

---
+++ user_authentication_service/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    basic Flask app
+"""
+from auth import Auth
+from flask import Flask, request, jsonify
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """
+    POST /users endpoint to register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
