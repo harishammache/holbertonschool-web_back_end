db35461fa8a2d43066549ae2ede931e7c7ba8976
harishammache <hammache_haris@hotmail.fr>
2025-06-26 17:12:46 +0200

    feat(auth): implement destroy_session method for logout

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,3 +71,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+            Destroy user session
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
