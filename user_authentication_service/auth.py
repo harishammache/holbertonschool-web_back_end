6c140f80cb1a565d1b645abce0cd04740b5be533
harishammache <hammache_haris@hotmail.fr>
2025-06-27 10:22:02 +0200

    feat(auth): add password reset token generation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -80,3 +80,15 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+            Generate a reset password token for a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError()
