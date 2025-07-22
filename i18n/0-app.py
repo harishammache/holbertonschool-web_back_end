7820f01858389428c65d13bfbde3fc2e7ce923de
harishammache <hammache_haris@hotmail.fr>
2025-07-22 13:59:47 +0200

    rename file

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index() -> str:
     """Renders the home page with a welcome message."""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
