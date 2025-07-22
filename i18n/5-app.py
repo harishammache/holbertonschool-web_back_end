33b690afadcab74f9d62bd3c4f2bdd3dc91245de
harishammache <hammache_haris@hotmail.fr>
2025-07-22 16:30:36 +0200

    feat(i18n): add user login simulation and locale support with Flask-Babel

---
+++ i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Basic Flask app with a single route."""
+
+
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+
+
+babel = Babel()
+
+
+class Config():
+    """that has a LANGUAGES class attribute"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+def get_locale():
+    """get_locale"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index() -> str:
+    """Renders the home page with a welcome message."""
+    return render_template('3-index.html', get_locale=get_locale)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    user_id = request.args.get('login_as')
+    if user_id and user_id.isdigit():
+        return users.get(int(user_id))
+    return None
+
+@app.before_request
+def before_request():
+    g.user = get_user()
+
+if __name__ == '__main__':
+    app.run()
