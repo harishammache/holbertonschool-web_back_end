6f9581adfd168f6b252dd084e0dfbf8700d9b7cd
harishammache <hammache_haris@hotmail.fr>
2025-07-29 14:19:36 +0200

    2022 to 2024

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam rock as their main style
 -- Uses attributes formed and split for computing the lifespan
 SELECT band_name, 
-       (IFNULL(split, 2022) - formed) AS lifespan
+       (IFNULL(split, 2024) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC; 
