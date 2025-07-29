941e597d41bfcc9770316df244be585efd22b8b8
harishammache <hammache_haris@hotmail.fr>
2025-07-29 16:07:13 +0200

    commentary

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
---Write a SQL script that creates an index
---table names and the first letter of name
+-- Write a SQL script that creates an index
+-- Table names and the first letter of name
 CREATE INDEX idx_name_first ON names(name(1));
