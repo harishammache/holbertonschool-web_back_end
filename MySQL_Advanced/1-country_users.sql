179d5af2a14602e05d95a40aa1483b2af07c031c
harishammache <hammache_haris@hotmail.fr>
2025-07-29 13:27:25 +0200

    feat: CREATE table users

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users
 -- If the table already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US",
