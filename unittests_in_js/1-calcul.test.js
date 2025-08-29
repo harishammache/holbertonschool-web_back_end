72a0b490288724452d826d4d6118482d2ab379a3
harishammache <hammache_haris@hotmail.fr>
2025-08-29 15:59:17 +0200

    feat: add calculateNumber function and corresponding unit tests

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describle('calculateNumber', () => {
+    
+    describe('SUM', () => {
+        it('result of two positiv number 1.4 + 3.5 should return 5', () => {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 5)
+        });
+
+        it('result of two negativ number -1.4 + -3.5 should return -5', () => {
+            assert.strictEqual(calculateNumber('SUM', -1.4, -4.5), -5)
+        })
+    })
+
+    describe('SUBSTRACT', () =>{
+        it('result of two positiv number 4 - 2 should return 2', () => {
+            assert.strictEqual(calculateNumber('SUBSTRACT', 4, 2), 2)
+        })
+
+        it('result of two negativ number (-3) - (-5) should return 2', () => {
+            assert.strictEqual(calculateNumber('SUBSTRACT', -3, -5), 2)
+        })
+    })
+
+    describe('DIVIDE', () => {
+        it('result of two positiv number 2 / 2 should return 1', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 2, 2), 1)
+        })
+
+        it('result of two negativ number -4 / -2 should return 2', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', -4, -2), 2)
+        })
+
+        it('result if b = 0 should return Error', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'ERROR')
+        })
+    })
+})
