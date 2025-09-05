2b9caa37e02ff0bab0597d5af3fd8576a944a9b3
harishammache <hammache_haris@hotmail.fr>
2025-09-05 14:22:46 +0200

    fix: add missing semicolon and wrap app.listen in a conditional

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
@@ -11,8 +11,10 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+  });
+}
 
 module.exports = app;
