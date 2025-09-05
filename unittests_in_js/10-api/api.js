ae6e66de24eea156de73b6b3656ae1c57e58f5b1
harishammache <hammache_haris@hotmail.fr>
2025-09-05 14:55:33 +0200

    fix: update cart route to enforce numeric ID parameter

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
+app.get('/cart/:id(\\d+)', (req, res) => {
   const { id } = req.params;
   res.send(`Payment methods for cart ${id}`);
 });
